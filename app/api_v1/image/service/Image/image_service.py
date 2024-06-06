ddfe174038c0561bfc04deb264a6a443d2b113c1
Jeongsik An <ajs15010120@gmail.com>
2024-06-06 17:14:54 +0900

    feat : delete real file

--- app/api_v1/image/service/Image/image_service.py
+++ app/api_v1/image/service/Image/image_service.py
@@ -1,3 +1,4 @@
+import os
 from typing import List, Optional
 from app.api_v1.image.dao.image_dao import ImageDao
 from app.api_v1.image.domain.model.image.ImageModel import Image
@@ -15,13 +16,17 @@ class ImageService:
         return created_image.to_schema()
 
     async def get_image_by_id(self, image_id: int) -> Optional[Image]:
-        return await (self.image_dao
-                      .get_image_by_id(image_id))
+        return await self.image_dao.get_image_by_id(image_id)
 
     async def get_all_images(self) -> List[Image]:
-        return await (self.image_dao
-                      .get_all_images())
+        return await self.image_dao.get_all_images()
 
     async def delete_image(self, image_id: int) -> bool:
-        return await (self.image_dao
-                      .delete_image(image_id))
+        return await self.image_dao.delete_image(image_id)
+
+    async def deleteFileAndTableDate(self, image_id: int):
+        image = await self.get_image_by_id(image_id)
+        if image is None:
+            raise ValueError(f"Image with id {image_id} not found")
+        await Image.from_delete_file(image.url)
+        return await self.delete_image(image_id)
